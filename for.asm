5c8c3f437859d67ccda8770fedb5d7e5552a815b
Nicholas <nnmarques97@gmail.com>
2019-06-25 12:11:45 -0300

    atualizado for com incremento dinamico

--- for.asm
+++ for.asm
@@ -11,18 +11,18 @@
     int   80h
 %endmacro
 
-%macro for 1
+%macro for 3
     mov edi, 1
     ;mov ecx, %1
     innerLoop:
 
         write_string loopmessage, 15
         ;loop innerLoop
-        add edi, 1
+        add edi, %2
         cmp edi, %1
-        jne innerLoop
+        jle innerLoop
     done:
-        mov   eax, 4
+        mov   eax, %2
         mov   ebx, 1
         mov   ecx, message
         mov   edx, 9
@@ -34,7 +34,7 @@
 
 %endmacro
 
-For: for 0x8
+For: for 0x8, 1,0x1
 
         section   .data
 message:  db        "Cafebabe", 10      ; note the newline at the end
